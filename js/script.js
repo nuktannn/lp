5e869118f161dd01832f43fd4250bf42c8169fbc
nuktannn <102274957+nuktannn@users.noreply.github.com>
2022-03-24 13:03:03 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+ //drawer
+  jQuery('.drawer-icon').on('click', function(e){
+   e.preventDefault()
+
+   jQuery('.drawer-icon').toggleClass('is-active');
+    jQuery('.drawer-icon-bars').toggleClass('is-active');
+   jQuery('.drawer-content').toggleClass('is-active');
+   jQuery('.drawer-background').toggleClass('is-active');
+   
+   return false;
+  });
